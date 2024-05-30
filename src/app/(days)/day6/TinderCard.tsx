da03c74d9490e3ff168fa0fcb0e7a96d69419768
Kwasi633 <ezrakwasi633@gmail.com>
2024-05-30 05:01:06 +0000

    Finish tinker swipe cards

--- src/app/(days)/day6/TinderCard.tsx
+++ src/app/(days)/day6/TinderCard.tsx
@@ -1,29 +1,107 @@
 import { View, Text, StyleSheet, Image, Dimensions} from 'react-native'
 import React from 'react'
-
 import { LinearGradient } from 'expo-linear-gradient'
+import Animated, { useDerivedValue, useAnimatedStyle, interpolate, SharedValue, useSharedValue, withSpring, withDecay} from 'react-native-reanimated';
+import {PanGesture} from 'react-native-gesture-handler'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+
+type TinkerCard = {
+  profile: {
+    image: string;
+    name: string;
+  };
+  numOfCard: number
+  curIndex: number;
+  activeIndex: SharedValue<number>;
+}
+
+const screenWidth = Dimensions.get('screen').width;
+export const tinderCardWidth = screenWidth * 0.8;
+
+const TinderCard = ({
+  profile,
+  numOfCard,
+  curIndex,
+  activeIndex
+}) => {
+
+  const translationX = useSharedValue(0);
 
 
+  const animatedCard = useAnimatedStyle(() => ({
+    opacity: interpolate(
+      activeIndex.value, 
+      [curIndex - 1, curIndex, curIndex + 1], 
+      [1 - 1 / 5, 1, 1]
+    ),
+    transform: [{
+      scale: interpolate(activeIndex.value, 
+        [curIndex - 1, curIndex, curIndex + 1], 
+        [0.95, 1, 1])
+    },
+    {translateY: interpolate(activeIndex.value,
+      [curIndex - 1, curIndex, curIndex + 1],
+      [-30 , 0, 0]
+    )},
+    {
+      translateX: translationX.value,
+    },
+    {
+      rotateZ: `${interpolate(
+        translationX.value,
+        [-screenWidth / 2, 0, screenWidth / 2],
+        [-15, 0, 15]
+      )}deg`
+    }
+  ]
+  }))
 
-export const tinderCardWidth = Dimensions.get('screen').width * 0.8;
+  const gesture = Gesture.Pan()
+  .onChange((event) => {
+    translationX.value = event.translationX;
 
-const TinderCard = (props) => {
+    activeIndex.value = interpolate(
+      Math.abs(translationX.value),
+      [0, 500],
+      [curIndex, curIndex + 0.8]
+    );
+
+  })
+  .onStart((event) => console.log('onStart: '))
+
+  .onEnd((event) => {
+    translationX.value = withSpring(0);
+
+    if(Math.abs(event.velocityX) > 400){
+    translationX.value = withDecay({ velocity: event.velocityX });
+
+    activeIndex.value = withSpring(curIndex + 1)
+    }
+  });
+
   return (
-    <View style={styles.card}>
+    <GestureDetector gesture={gesture}>
+    <Animated.View 
+    style={[
+      styles.card,
+      animatedCard, 
+    {zIndex: numOfCard - curIndex, 
+    }]
+    }>
 
-      <Image style={[StyleSheet.absoluteFillObject, styles.image]} source={props.profile.image}/>
+      <Image style={[StyleSheet.absoluteFillObject, styles.image]} source={profile.image}/>
       
       <LinearGradient
         // Background Linear Gradient
         colors={['transparent', 'rgba(0,0,0,0.8)', ]}
-        style={[StyleSheet.absoluteFillObject, {top: '50%'}, ]}
+        style={[StyleSheet.absoluteFillObject, {top: '50%'}]}
       />
-
       <View style={styles.footer}>
-      <Text style={styles.name}>{props.profile.name}</Text>
+      <Text style={styles.name}>{profile.name}</Text>
       </View>
-    </View>
-  )
+    </Animated.View>
+  </GestureDetector>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -31,9 +109,9 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         overflow: 'hidden',
         justifyContent: 'flex-end',
-        height: 351,
+        height: 600,
         width: tinderCardWidth,
-
+        position: 'absolute',
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
@@ -41,11 +119,12 @@ const styles = StyleSheet.create({
         },
         shadowOpacity: 0.22,
         shadowRadius: 2.22,
-        elevation: 5
-    },
+        elevation: 5,
+      },
 
     image: {
-      borderRadius: 15
+      borderRadius: 15,
+      height: 600
     },
 
     footer: {
@@ -59,3 +138,23 @@ const styles = StyleSheet.create({
 })
 
 export default TinderCard;
+
+// const gesture = Gesture.Pan()
+// .onBegin((event) => console.log('onBegin: '))
+// .onFinalize((event) => console.log('onFinalize: '))
+// .onChange((event) => {
+//   translationX.value = event.translationX;
+//   //activeIndex.value = interpolate();
+// })
+// .onUpdate((event) => console.log('onUpdate:', event))
+// .onStart((event) => console.log('onStart: '))
+
+// .onEnd((event) => {
+//   translationX.value = withSpring(0);
+
+//   if(Math.abs(event.velocityX) > 400){
+//   translationX.value = withDecay({ velocity: event.velocityX });
+
+//   activeIndex.value = activeIndex.value - 1;
+//   }
+// })
--- src/app/(days)/day6/tinder.tsx
+++ src/app/(days)/day6/tinder.tsx
@@ -1,46 +1,78 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Stack } from 'expo-router';
 import TinderCard from './TinderCard'
-import Lady from '../../../../assets/lady.jpg';
+import Zee from '../../../../assets/Zee.jpg';
+import Amina from '../../../../assets/Amina.jpg';
+import NanaAma from '../../../../assets/NanaAma.jpg';
+import ChanLee from '../../../../assets/ChanLee.jpg';
+import Annie from '../../../../assets/Annie.jpg';
+
+import { runOnJS ,useAnimatedReaction, useDerivedValue, useSharedValue, withDecay, withSpring } from 'react-native-reanimated';
 
 const profiles = [
   {
     id: 1,
-    image: Lady,
-    name: 'Diana'
-  },
-  {
-    id: 2,
-    image: Lady,
+    image: Zee,
     name: 'Zee'
   },
   {
+    id: 2,
+    image: Annie,
+    name: 'Annie'
+  },
+  {
     id: 3,
-    image: Lady,
-    name: 'Wendy'
+    image: NanaAma,
+    name: 'Nana Ama'
   },
   {
     id: 4,
-    image: Lady,
-    name: 'Rita'
+    image: ChanLee,
+    name: 'Chan Lee'
   },
   {
     id: 5,
-    image: Lady,
-    name: 'Vera'
+    image: Amina,
+    name: 'Amina'
   },
   
 ];
 
 const Tinder = () => {
+  const activeIndex = useSharedValue(0);
+  const [index, setIndex] = useState(0);
+  const [user, setUser] = useState(profiles)
+
+  useAnimatedReaction(
+    () => activeIndex.value, 
+    (value, preValue) => {
+    if (Math.floor(value) != index){
+      runOnJS(setIndex)(Math.floor(value));
+    }
+}
+);
+
+  useEffect(() => {
+    if(index > profiles.length - 3){
+      setUser((user) => [...user, ...profiles.reverse()])
+    }
+  }, [index])
+    
   return (
-    <View>
-      {profiles.map((profile) => (
+    
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Stack.Screen options={{ headerShown: false}}/>
+
+      {profiles.map((profile, index) => (
       <TinderCard 
-      key={profile.id}
-      profile={profile}/>  
-      ))}
-      
+      key={`${user.id} - ${index}`}
+      profile={profile}
+      numOfCard={profiles.length}
+      curIndex={index}
+      activeIndex={activeIndex}
+      />  
+      ))}   
     </View>
   )
 }
